7643de3bcb167567ffa82c06f2645a5c77b8ca25
Cyril Pierro <fiopapa32@gmail.com>
2022-09-08 12:36:10 +0000

    add store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,9 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import studentSlice from '../features/students/studentSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    students: studentSlice
   },
 });
 
